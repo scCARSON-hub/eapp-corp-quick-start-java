9e95d74659b3320e85b518354ccf74605c067022
scCARSON-hub <62631862+scCARSON-hub@users.noreply.github.com>
2020-03-25 14:45:58 +0800

    Update and rename src/main/java/com/config/Constant.java to SRC/主/爪哇/COM/配置/Constant.java

--- SRC/主/爪哇/COM/配置/Constant.java
+++ SRC/主/爪哇/COM/配置/Constant.java
@@ -7,9 +7,9 @@ public class Constant {
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
      */
-    public static final String APP_KEY = "***";
+    public static final String APP_KEY = "dingguhtnqwk4whmgkha";
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
      */
-    public static final String APP_SECRET="***";
+    public static final String APP_SECRET="0Hr9GG7GrF0defzdGozQNFsVjFezZyuuNhRa5BtbDonbJN-kUBRR3PqfPWyfS5oU";
 }
